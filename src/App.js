12f257eb0c6d40f6cdccb663462d843bba9ec79d
Dastagir2k <dastagir2k@gmail.com>
2023-12-11 13:04:23 +0530

    Initial commit for frontend

--- src/App.js
+++ src/App.js
@@ -1,112 +1,36 @@
-// import './App.css';
-// import {useState} from "react";
-// import Axios from "axios"
-// function App() {
-//   const [name,setName]=useState('');
-//   const [email,setEmail]=useState('');
-//   const [gender,setGender]=useState('');
-//   const [employee,setEmployee]=useState([]);
-//   // const displayInfo=()=>{
-//   //   console.log(name+email+gender);
-//   // }
-
-
-//   const AddEmployee=(e)=>{
-//     e.preventDefault();
-//     Axios.post('http://localhost:3002/create',
-//     {
-//       name:name,
-//       email:email,
-//       gender:gender,
-//     }).then(()=>{
-//       console.log('Success');
-//     }).catch((err)=>{
-//       console.log("Errrorrrrr"+err);
-//     })
-//   }
-
-//   const ShowEmployee = ()=>{
-//     Axios.get('http://localhost:3002/show')
-//     .then((result)=>{
-//       setEmployee(result.data);
-//       console.log(result);
-//     }).catch((err)=>{
-//       console.log("Error occur while display"+err);
-//     })
-//   }
-
-//   return (
-//     <div className='App'>
-//       <div className='info'>
-//         <label>Name :</label>
-//         <input type='text'
-//         onChange={(e)=>{setName(e.target.value)}}/>
-//         <label>Email :</label>
-//         <input type='email'
-//         onChange={(e)=>{setEmail(e.target.value)}}/>
-//         <label>Gender :</label>
-//         <input type='text'
-//         onChange={(e)=>{setGender(e.target.value)}}/>
-//         <button onClick={AddEmployee}>Add Employee</button>
-//       </div>
-//       <hr/>
-//       <div className='employee'>
-//         <button onClick={ShowEmployee}>Show Employee</button>
-//         {employee.map((val, id) => {
-//           return <div key={id} className='employee'>
-//             <h2>{val.name}</h2>
-//             <h2>{val.email}</h2>
-//             <h2>{val.gender}</h2>
-//           </div>;
-//         })}
-//       </div>
-//       {/* <div className='employee'>
-//         <button onClick={ShowEmployee}>Shoe Employee</button>
-//         <div>
-//           {employee.map((data,id)=>(
-//             <div key={id}>
-//               <div>{data.name}</div>
-//               <div>{data.email}</div>
-//               <div>{data.gender}</div>
-//             </div>
-//           ))}
-//         </div>
-//       </div> */}
-      
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import './App.css';
+// import "./App.css";
 import { useState } from "react";
 import Axios from "axios";
 
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [gender, setGender] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [department, setDepartment] = useState("");
+  const [dob, setDob] = useState("");
   const [employee, setEmployee] = useState([]);
 
   const AddEmployee = (e) => {
     e.preventDefault();
-    Axios.post('http://localhost:3002/create', {
+    Axios.post("http://localhost:3002/create", {
       name: name,
       email: email,
       gender: gender,
+      designation: designation,
+      dob: dob,
+      department: department,
     })
       .then(() => {
-        console.log('Success');
+        console.log("Success");
       })
       .catch((err) => {
         console.log("Error: " + err);
       });
-  }
+  };
 
   const ShowEmployee = () => {
-    Axios.get('http://localhost:3002/show')
+    Axios.get("http://localhost:3002/show")
       .then((result) => {
         setEmployee(result.data);
         console.log(result);
@@ -114,29 +38,134 @@ function App() {
       .catch((err) => {
         console.log("Error occur while displaying: " + err);
       });
-  }
+  };
 
   return (
-    <div className='App'>
-      <div className='info'>
-        <label>Name :</label>
-        <input type='text' onChange={(e) => { setName(e.target.value) }} />
-        <label>Email :</label>
-        <input type='email' onChange={(e) => { setEmail(e.target.value) }} />
-        <label>Gender :</label>
-        <input type='text' onChange={(e) => { setGender(e.target.value) }} />
-        <button onClick={AddEmployee}>Add Employee</button>
+    <div className="App">
+      <h1 className="mb-4 text-center">Employee Management System</h1>
+      <div className="container">
+        <div className="row justify-content-center mt-4 ">
+          <div className="col-md-6 d-flex">
+            <div className="col md-3 p-5 border rounded bg-primary">
+              <label  className="col h3 ">Name :</label>
+              <input
+              required
+                type="text"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                className="p-1 mt-2 w-10"
+              />
+              <br />
+              <label className="h3">Email :</label>
+              <input
+                required
+                type="email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                className="p-1 mt-2 w-10"
+              />
+              <br />
+              <label className="h3">Gender :</label>
+              <input
+              required
+                type="text"
+                onChange={(e) => {
+                  setGender(e.target.value);
+                }}
+                className="p-1 mt-2 w-8"
+              />
+              <br />
+              <label className="col h3">Department :</label>
+              <input
+              required
+                type="text"
+                onChange={(e) => {
+                  setDepartment(e.target.value);
+                }}
+                className="p-1 mt-2"
+              />
+              <br />
+              <label className="h3">Designation :</label>
+              <input
+              required
+                type="text"
+                onChange={(e) => {
+                  setDesignation(e.target.value);
+                }}
+                className="p-1 mt-2"
+              />
+              <br />
+              <label className="h3">DOB :</label>
+              <input
+                type="date"
+                onChange={(e) => {
+                  setDob(e.target.value);
+                }}
+                className="p-1 mt-2"
+              />
+              <br />
+              <div className="text-center">
+                <button
+                  onClick={AddEmployee}
+                  className="btn btn-success btn-priamry  p-2 mt-4 w-5 h-20"
+                >
+                  Add Employee
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <hr />
-      <div className='employees'>
-        <button onClick={ShowEmployee}>Show Employee</button>
-        {employee.map((val, index) => (
+      <div className="employees">
+        <div className="text-center">
+          <button
+            onClick={ShowEmployee}
+            className="btn btn-block btn-primary w-20"
+          >
+            Show Employee
+          </button>
+        </div>
+        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+          <div className="w-50 rounded ">
+            <table className="table ">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Gender</th>
+                  <th>dob</th>
+                  <th>Department</th>
+                  <th>Designation</th>
+                </tr>
+              </thead>
+              <tbody>
+                {employee.map((val, index) => (
+                  <tr key={index} className="employee">
+                    <td>{val.name}</td>
+                    <td>{val.email}</td>
+                    <td>{val.gender}</td>
+                    <td>{val.dob}</td>
+                    <td>{val.department}</td>
+                    <td>{val.designation}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        {/* {employee.map((val, index) => (
   <div key={index} className='employee'>
     <h2>Name: {val.name}</h2>
     <h2>Email: {val.email}</h2>
     <h2>Gender: {val.gender}</h2>
+    <h2>DOB: {val.dob}</h2>
+    <h2>Department: {val.department}</h2>
+    <h2>Designation: {val.designation}</h2>
   </div>
-))}
+))} */}
       </div>
     </div>
   );
